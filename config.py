2072c55b89be3d4ab3a795a89493e7921a09b38e
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2025-01-18 23:28:50 +0700

    .env file is avaliable

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List
 
 
 class Settings(BaseSettings):
     origin: List[str] = [
-        "http://127.0.0.1:3000",  # React default port
-        "http://localhost:3000",  # React default port
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
     ]
     video_path: str = "/videos",
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
